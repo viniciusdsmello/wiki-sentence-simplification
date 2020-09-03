ecae133603e155b7d2d22c089c719bb40aa0f30d
Vinícius dos Santos Mello <viniciusdsmello@poli.ufrj.br>
2020-09-03 12:44:43 -0300

    Fixed the division and module in beam_search

--- nmt_model.py
+++ nmt_model.py
@@ -435,7 +435,7 @@ class NMT(nn.Module):
             contiuating_hyp_scores = (hyp_scores.unsqueeze(1).expand_as(log_p_t) + log_p_t).view(-1)
             top_cand_hyp_scores, top_cand_hyp_pos = torch.topk(contiuating_hyp_scores, k=live_hyp_num)
 
-            prev_hyp_ids = torch.div(top_cand_hyp_pos, len(self.vocab.tgt))
+            prev_hyp_ids = torch.true_divide(top_cand_hyp_pos, len(self.vocab.tgt))
             hyp_word_ids = torch.fmod(top_cand_hyp_pos, len(self.vocab.tgt))
 
             new_hypotheses = []
