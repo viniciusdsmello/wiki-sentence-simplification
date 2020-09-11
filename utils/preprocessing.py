55878881306f3fe81fa810f2800085e17a51345d
Vinícius dos Santos Mello <viniciusdsmello@poli.ufrj.br>
2020-09-10 23:46:23 -0300

    Changed preprocessing steps

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -13,20 +13,29 @@ def remove_accented_chars(text):
 # function to remove special characters
 def remove_special_characters(text):
     # define the pattern to keep
-    pat = r'[^a-zA-z0-9.,!?/:;\"\'\s]' 
+    pat = r'[^a-zA-z0-9.,!?/:;\'\"\`\s]' 
     return re.sub(pat, '', text)
 
 def remove_extra_whitespace_tabs(text):
     pattern = r'^\s*|\s\s*'
     return re.sub(pattern, ' ', text).strip()
 
+def replace_quotes(text):
+    pattern = r'(["\'`]){2,}'
+    return re.sub(pattern, '\"', text).strip()
+
 def to_lowercase(text):
     return text.lower()
 
-def preprocessing(x):
-    x = remove_html_tags(str(x))
-    x = remove_special_characters(x)
+def preprocessing(x: str):
     x = remove_accented_chars(x)
+    x = remove_special_characters(x)
     x = remove_extra_whitespace_tabs(x)
+    x = replace_quotes(x)
     x = to_lowercase(x)
+    x = x.strip()
     return x
+
+if __name__ == "__main__":
+    text = "eddie hazel is listed at 43 in rolling stone magazine 's `` 100 greatest guitarists of all time '' issue ."
+    print(preprocessing(text))
--- utils/split_data.py
+++ utils/split_data.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import csv
 from sklearn.model_selection import train_test_split
 
 from preprocessing import preprocessing
@@ -8,11 +9,11 @@ current_dir = os.path.dirname(__file__)
 data_dir = os.path.join(os.path.abspath(os.path.join(current_dir, os.pardir)), 'data')
 
 header = ['article_title', 'paragraph_number', 'sentence']
-normal_df = pd.read_csv(os.path.join(data_dir, 'normal.aligned'), delimiter='\t\t', names=header)
-simple_df = pd.read_csv(os.path.join(data_dir, 'simple.aligned'), delimiter='\t\t', names=header)
+normal_df = pd.read_csv(os.path.join(data_dir, 'normal.aligned'), delimiter='\t', names=header)
+simple_df = pd.read_csv(os.path.join(data_dir, 'simple.aligned'), delimiter='\t', names=header)
 
-normal_df['sentence'] = normal_df['sentence'].apply(lambda x: preprocessing(x))
-simple_df['sentence'] = simple_df['sentence'].apply(lambda x: preprocessing(x))
+normal_df['sentence'] = normal_df['sentence'].astype(str).apply(lambda x: preprocessing(x))
+simple_df['sentence'] = simple_df['sentence'].astype(str).apply(lambda x: preprocessing(x))
 
 RANDOM_STATE = 9
 
@@ -20,7 +21,16 @@ X_train, X_test, y_train, y_test = train_test_split(normal_df.sentence, simple_d
                                                     test_size=0.10, random_state=RANDOM_STATE)
 
 # Write CSVs with splited data
-X_train.to_csv(os.path.join(data_dir, 'normal.train'), index=False, header=False)
-X_test.to_csv(os.path.join(data_dir, 'normal.dev'), index=False, header=False)
-y_train.to_csv(os.path.join(data_dir, 'simple.train'), index=False, header=False)
-y_test.to_csv(os.path.join(data_dir, 'simple.dev'), index=False, header=False)
+X_train.to_csv(os.path.join(data_dir, 'normal.train'), index=False, header=False, quoting=csv.QUOTE_NONE, sep='\t')
+X_test.to_csv(os.path.join(data_dir, 'normal.dev'), index=False, header=False, quoting=csv.QUOTE_NONE, sep='\t')
+y_train.to_csv(os.path.join(data_dir, 'simple.train'), index=False, header=False, quoting=csv.QUOTE_NONE, sep='\t')
+y_test.to_csv(os.path.join(data_dir, 'simple.dev'), index=False, header=False, quoting=csv.QUOTE_NONE, sep='\t')
+
+test_normal_df = pd.read_csv(os.path.join(data_dir, 'normal.test'), delimiter='\t', names=['sentence'])
+test_simple_df = pd.read_csv(os.path.join(data_dir, 'simple.test'), delimiter='\t', names=['sentence'])
+
+test_normal_df['sentence'] = test_normal_df['sentence'].astype(str).apply(lambda x: preprocessing(x))
+test_simple_df['sentence'] = test_simple_df['sentence'].astype(str).apply(lambda x: preprocessing(x))
+
+test_normal_df.to_csv(os.path.join(data_dir, 'normal.test'), index=False, header=False, quoting=csv.QUOTE_NONE, sep='\t')
+test_simple_df.to_csv(os.path.join(data_dir, 'simple.test'), index=False, header=False, quoting=csv.QUOTE_NONE, sep='\t')
