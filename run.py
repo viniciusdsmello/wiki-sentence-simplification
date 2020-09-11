a5ed07e8ffcda33eb3d599412e057ee0f4b0f928
Vinícius dos Santos Mello <viniciusdsmello@poli.ufrj.br>
2020-09-10 23:52:04 -0300

    Added on more option to run.py

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ Usage:
 Options:
     -h --help                               show this screen.
     --cuda                                  use GPU
+    --cuda-device                           GPU ID [default: 0]
     --train-src=<file>                      train source file
     --train-tgt=<file>                      train target file
     --dev-src=<file>                        dev source file
